bb149ebb17ca5d03e6c3e4319a64262e29bb6ce1
dongjiancheng <dongjiancheng@meituan.com>
2023-05-27 13:06:29 +0800

    pre file

---
+++ src/components/001-page/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="pithy-switch">
+        <a-switch :checked="checked" @change="handleChange" />
+    </div>
+</template>
+  
+<script>
+import { defineComponent } from 'vue';
+import { Switch } from 'ant-design-vue';
+
+export default defineComponent({
+    name: 'PithySwitch',
+    components: {
+        'a-switch': Switch,
+    },
+    data() {
+        return {
+            checked: false,
+        };
+    },
+    methods: {
+        handleChange(checked) {
+            this.checked = checked;
+        },
+    },
+});
+</script>
+  
+<style scoped>
+.pithy-switch {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+}
+</style>
